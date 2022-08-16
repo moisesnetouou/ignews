d37c630f9f22eb279a008851ba0013b9c868a904
Moisés <moisesnetored@gmail.com>
2022-08-16 14:49:08 -0400

    feat: Using Static Site Generation.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
@@ -35,7 +35,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1LXU1KE5uVVcrnb7A6hP57ZF', { //Api do produto
     expand: ['product']
   })
@@ -51,6 +51,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas (60 segundos * 60 para 1 hora, e por fim 24 para 24 horas)
   }
 }
