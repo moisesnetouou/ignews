9d5d514be96c839aca5a38c3c27d01efe15fb2bf
Moisés <moisesnetored@gmail.com>
2022-08-16 13:42:43 -0400

    feat: Home page created.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
-      <Head><title>Início | ig.news</title></Head>
-      <h1>Hello <span>World</span></h1>
+      <Head><title>Home | ig.news</title></Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏🏻Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   )
 }
