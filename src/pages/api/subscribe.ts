a5165e46ff8181151dcd0d75bba81c04da6fa4eb
Moisés <moisesnetored@gmail.com>
2022-08-18 17:05:11 -0400

    feat: Avoiding Duplication in Stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,7 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { query as q } from 'faunadb';
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from 'next-auth/react'
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_customer_id: string;
+  }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -9,12 +20,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     //Criando customer dentro do painel do Stripe
     const session = await getSession({ req }) // Vamos precisar dos dados do usuário logado, nesse caso pegamos pela session
 
-    const stripeCustomer = await stripe.customers.create({ // Cadastro de cliente proprio
-      email: session.user.email,
-    })
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email)
+        )
+      )
+    )
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      const stripeCustomer = await stripe.customers.create({ // Cadastro de cliente proprio
+        email: session.user.email,
+      })
+
+      await fauna.query(
+        q.Update(
+          q.Ref(q.Collection('users'), user.ref.id),
+          {
+            data: {
+              stripe_customer_id: stripeCustomer.id
+            }
+          }
+        )
+      )
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id, //id do customer no stripe
+      customer: customerId, //id do customer no stripe
       payment_method_types: ['card'], // Metodo de pagamento (c. credito)
       billing_address_collection: 'required', // Se quer obrigar o user a add endereço ou deixar o stripe lidar com isso
       line_items: [ // Quais itens queremos que a pessoa possa ter dentro do carrinho
